2af00947c92d3d65d9b310b3f38e34e58cea2b3b
asadmukhtarr <axad03213@gmail.com>
2025-09-05 19:41:21 +0500

    sign up functionality with email already exists

---
+++ php/actions/cn.php
@@ -0,0 +1,4 @@
+<?php
+    // connection code ..
+     $cn = mysqli_connect('localhost','root','','fswd35ls') or die('cant connect with database');
+?>
--- php/actions/register.php
+++ php/actions/register.php
@@ -4,14 +4,25 @@
     $whatsapp = $_POST['whatsapp']; // whatsapp ..
     $password = $_POST['password']; // password ..
     $cpassword = $_POST['confirm_password']; // cofirm password ..
-    $gender = $_POST['gender'];
+    $gender = $_POST['gender']; // gender ..
+    // testing for same password with confirmation ..
     if($password == $cpassword){
         // for connection ...
-        $cn = mysqli_connect('localhost','root','','fswd35ls') or die('cant connect with database');
-        $query = "INSERT INTO `users`(name,email,whatsapp,password,gender) VALUES ('$name','$email','$whatsapp','$password','$gender')";
-        mysqli_query($cn,$query) or die(mysqli_error($cn));
-        $error = "Thank you for register";
-        header('Location:../register.php?success='.$error);
+        include('actions/cn.php'); // connectoin file ...
+        $que = "SELECT * FROM `users` WHERE email = '$email'"; // its finding data from database table against email ..
+        $result = mysqli_query($cn,$que) or die('cant run query'); // its runing queries ..
+        $rows = mysqli_num_rows($result); // number count ..
+       // echo $rows; // for testing ...
+        if($rows > 0){
+            $error = "Email Already Exists";
+            header('Location:../register.php?error='.$error);   
+        } else {
+            $query = "INSERT INTO `users`(name,email,whatsapp,password,gender) VALUES ('$name','$email','$whatsapp','$password','$gender')";
+            mysqli_query($cn,$query) or die(mysqli_error($cn));
+            $error = "Thank you for register";
+            header('Location:../register.php?success='.$error);
+        }
+        
     } else {
         $error = "Password did not match";
         header('Location:../register.php?error='.$error);
