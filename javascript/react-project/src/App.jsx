07c15c7dcc13e1b4e1bb82646eb499a11928fd2f
asadmukhtarr <axad03213@gmail.com>
2025-11-22 19:40:57 +0500

    react component

---
+++ javascript/react-project/src/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./components/Home";
+import About from "./components/About";
+import Products from "./components/Products";
+import Contact from "./components/Contact";
+function App(){
+    return (
+        <div>
+            <h2>Main Component | Hello World</h2>
+            <p>Loem Asadjhaskjb asdkjahdshsadkjhakjdsh</p>
+            <Home />
+            <About />
+            <Products />
+            <Contact />
+        </div>
+    );
+}
+export default App;
---
+++ javascript/react-project/src/components/Products.jsx
@@ -0,0 +1,8 @@
+const Products = () => {
+    return (
+        <div>
+            <h2>Helo Products</h2>
+        </div>
+    )
+}
+export default Products;
